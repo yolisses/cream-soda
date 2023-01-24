ffdac1e2897eff4c670f8ae3c76c3ae4b561ae14
Ulisses <iamurise@gmail.com>
2023-01-24 13:11:00 -0300

    remove js files

--- env.js
+++
@@ -1,7 +0,0 @@
-import { config } from "dotenv";
-config();
-
-const { SERVER_SSH_URL, SERVER_SSH_PORT, PASSWORD, USERNAME, HOST_SSH_URL } =
-  process.env;
-
-export { SERVER_SSH_URL, SERVER_SSH_PORT, PASSWORD, USERNAME, HOST_SSH_URL };
--- servers.js
+++
@@ -1,1 +0,0 @@
-export const servers = ["nginx", "apache2"];
